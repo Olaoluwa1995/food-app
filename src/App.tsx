03ef78f6919ad11a1d3235a4a5c1e0670de42dbe
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-08 08:45:55 +0100

    App navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { CSSReset, ChakraProvider, theme, Switch } from "@chakra-ui/react";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={{ ...theme }}>
+      <CSSReset />
+      <Switch>
+        {/* <Route exact path='/' component={} />
+       <Route exact path='/sign-up' component={} /> */}
+      </Switch>
+    </ChakraProvider>
   );
 }
 
